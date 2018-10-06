9f5c65b343be76593322f420b3f149dffb587b78
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2018-10-07 02:48:33 +0530

    Adding Child Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChildComponent } from './child/Child.Component';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/child/Child.Component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-child',
+    templateUrl: 'Child.Component.html'
+})
+export class ChildComponent {
+    Childmessage = 'Hello Child';
+}
